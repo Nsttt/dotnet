b52cc1f7626d4313ff97166238115c09b35d49a5
Nestor <nestor@nstlopez.com>
2024-04-07 15:34:34 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿#region 1. Minimal Web API
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+#endregion
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello, World from Web API!");
+
+app.Run();
